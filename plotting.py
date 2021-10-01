b0ce19da0bc751cb6873ed1e5872a8e6e6851f45
1smay3 <spencermayy@gmail.com>
2021-10-01 16:08:31 +0100

    Efficient frontier logic

--- plotting.py
+++ plotting.py
@@ -1,18 +1,45 @@
-from data import *
+from efficientFrontier import mean_variance_pairs
+import plotly.graph_objects as go
+import plotly.express as px
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import math
 
-cr_list = []
-for factor in factor_returns:
-    cr = factor_returns[factor]['cum_ret']
-    cr.name=factor
-    cr_list.append(cr)
+# Plot the risk vs. return of randomly generated portfolios
+# Manipulate mean_variance_pairs to plot
+mvp = pd.DataFrame.from_records(mean_variance_pairs)
+mvp.rename({0:'mean', 1:'variance'}, axis=1, inplace=True)
 
-pd.concat(cr_list, axis=1).plot()
-plt.show()
+# sqrt variance for std
+mvp['std'] = mvp['variance'].apply(math.sqrt)
+
+# Risk free rate for sharpe ratio
+rf =0
 
 
-# data_you_need=pd.DataFrame()
-# for infile in glob.glob("*.xlsx"):
-#     data = pandas.read_excel(infile)
-#     data_you_need=data_you_need.append(data,ignore_index=True)
+
+print("Debug")
+# Create Plot
+# fig = go.Figure()
+# fig.add_trace(go.Scatter(x=mean_variance_pairs[:,1]**0.5,
+#                          y=mean_variance_pairs[:,0],
+#                       #- Add color scale for sharpe ratio
+#                       marker=dict(color=(mean_variance_pairs[:,0]-rf)/(mean_variance_pairs[:,1]**0.5),
+#                                   showscale=True,
+#                                   size=7,
+#                                   line=dict(width=1),
+#                                   colorscale="RdBu",
+#                                   colorbar=dict(title="Sharpe<br>Ratio")
+#                                  ),
+#                       mode='markers'))
+#
+#
+#
+# # Add title/labels
+# fig.update_layout(template='plotly_white',
+#                   xaxis=dict(title='Annualised Risk (Volatility)'),
+#                   yaxis=dict(title='Annualised Return'),
+#                   title='Sample of Random Portfolios',
+#                   coloraxis_colorbar=dict(title="Sharpe Ratio"))
+#
+# fig.show()
